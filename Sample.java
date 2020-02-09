797e530b7f9ff92a09865efeb6bad571f40ac98e
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-02-08 19:24:34 -0800

    committing queue implementation

--- Sample.java
+++ Sample.java
@@ -5,3 +5,22 @@
 
 
 // Your code here along with comments explaining your approach
+
+1) Design HashSet
+
+
+
+2) Implement Queue using Stacks
+
+Time Complexity :  push() -> O(1)
+					pop() -> O(N)
+					peek() -> O(N)
+					IsEmpty() -> O(1)
+Space Complexity : O(N) since we are using another data structure for calculations
+Did this code successfully run on Leetcode : Yes
+
+Approach:
+
+1. Lets take two stacks, 1 for push and another one for pop & peek
+2. for every push operation, we push the elements in stack1
+3. for every pop operation, check if stack2 is empty then transfer all the elements from stack1 to stack2 and then pop from stack2.
